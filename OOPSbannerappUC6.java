3b25c58eb273f8045eaed5510ee96b1c3161d88d
ak8532-hub <ak8532@srmist.edu.in>
2026-03-10 13:46:59 +0530

    Add files via upload

---
+++ OOPSbannerappUC6.java
@@ -0,0 +1,60 @@
+public class OOPSbannerappUC6 {
+
+    static String[] getOPattern() {
+        return new String[] {
+            "   ***   ",
+            " **   **  ",
+            "**     ** ",
+            "**     ** ",
+            "**     ** ",
+            "**     ** ",
+            "**     ** ",
+            " **   **  ",
+            "   ***   "
+        };
+    }
+
+    static String[] getPPattern() {
+        return new String[] {
+            "   ******   ",
+            "**    ** ",
+            "**    ** ",
+            "**    ** ",
+            "******   ",
+            "**       ",
+            "**       ",
+            "**       ",
+            "  **       "
+        };
+    }
+
+    static String[] getSPattern() {
+        return new String[] {
+            "  *****  ",
+            " **       ",
+            "**        ",
+            " **       ",
+            "  ***     ",
+            "    **   ",
+            "      ** ",
+            "    **   ",
+           "*****    "
+        };
+    }
+
+    public static void main(String[] args) {
+
+        String[] O = getOPattern();
+        String[] P = getPPattern();
+        String[] S = getSPattern();
+
+        for (int i = 0; i < O.length; i++) {
+            System.out.println(
+                O[i] + " " +
+                O[i] + " " +
+                P[i] + " " +
+                S[i]
+            );
+        }
+    }
+}
