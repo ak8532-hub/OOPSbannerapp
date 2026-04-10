435c5e81d15e7068d954d163813b1f33dc6c4c94
ak8532-hub <ak8532@srmist.edu.in>
2026-04-10 15:48:17 +0530

    UC3-IMPLEMENT

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,13 +1,33 @@
+/**
+ * OOPSBannerApp - UC3
+ * This program prints "OOPS" in banner format using String.join()
+ * 
+ * @author Anupam
+ * @version 3.0
+ */
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        System.out.println(" *****   *****   ******   ***** ");
-        System.out.println("*     * *     *  *     * *     *");
-        System.out.println("*     * *     *  *     * *      ");
-        System.out.println("*     * *     *  ******   ***** ");
-        System.out.println("*     * *     *  *            *");
-        System.out.println("*     * *     *  *      *     *");
-        System.out.println(" *****   *****   *       ***** ");
+        System.out.println(String.join(" ",
+                " ***** ", " ***** ", " ****** ", " ***** "));
+
+        System.out.println(String.join(" ",
+                "*     *", "*     *", "*     *", "*     *"));
+
+        System.out.println(String.join(" ",
+                "*     *", "*     *", "*     *", "*"));
+
+        System.out.println(String.join(" ",
+                "*     *", "*     *", "******", " ***** "));
+
+        System.out.println(String.join(" ",
+                "*     *", "*     *", "*", "      *"));
+
+        System.out.println(String.join(" ",
+                "*     *", "*     *", "*", "*     *"));
+
+        System.out.println(String.join(" ",
+                " ***** ", " ***** ", "*", " ***** "));
     }
 }
